f4e7c33b9fa261f1ea9e656db664e1f16ae401c9
jaredDelao <jaredhm07@gmail.com>
2020-06-18 12:32:32 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/@components/home/home.component';
+import { ProductDetailComponent } from './pages/@components/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,

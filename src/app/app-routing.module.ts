fd67f014a780027579c2d7e13aed47c2e734230b
jaredDelao <jaredhm07@gmail.com>
2020-07-03 17:55:37 -0500

    rutas e imagenes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './pages/@components/product-detail/product-detail.component';
+import { HomeComponent } from './pages/@components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'product-detail', component: ProductDetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

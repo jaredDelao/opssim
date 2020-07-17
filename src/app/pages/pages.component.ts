4ac537aa57f601a97d316e74272ae1f2cb00c087
jaredDelao <jaredhm07@gmail.com>
2020-07-16 22:54:59 -0500

    cart box

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, ViewContainerRef } from '@angular/core';
 import { MenuSign } from '../models/enum.model';
 import { MatDialog } from '@angular/material';
 import { SignupComponent } from '../shared/@modals/signup/signup.component';
 import { SigninComponent } from '../shared/@modals/signin/signin.component';
 import { SidenavService } from '../services/sidenav.service';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-pages',
@@ -25,9 +26,35 @@ import { SidenavService } from '../services/sidenav.service';
         <!-- menu user -->
         <!-- <button mat-button class="toolbar-menu__button" [matMenuTriggerFor]="beforeMenu">Before</button> -->
 
-        <img matRipple src="../../assets/images/svg/user.svg" [matMenuTriggerFor]="beforeMenu">
-        <img matRipple src="../../assets/images/svg/cart-white.svg" [routerLink]="['/cart']">
+        <img class="toolbar-menu__img-icon" matRipple src="../../assets/images/svg/user.svg" [matMenuTriggerFor]="beforeMenu">
 
+        <!-- float cart -->
+        <div class="menuCart">
+          <img class="toolbar-menu__img-icon" matRipple src="../../assets/images/svg/cart-white.svg" [routerLink]="['/cart']">
+
+          <div class="menuCart__content">
+            <div class="menuCart__box-cart" #floatCart>
+              <div class="menuCart__item" *ngFor="let item of itemsDummy">
+                <div class="menuCart__image">
+                  <img class="menuCart__img" [src]="item.img[0]">
+                </div>
+                <div class="menuCart__info">
+                  <div class="menuCart__title">
+                    <span>{{ item.name }}</span>
+                  </div>
+                  <div class="menuCart__description">
+                    <span class="menuCart__quantity mr-3">x{{ item.stock }}</span>
+                    <span class="menuCart__price">{{ item.price }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        
+
+        <!-- float actions user -->
         <mat-menu #beforeMenu="matMenu" xPosition="before">
           <button mat-menu-item (click)="openModal('signIn')">Iniciar sesión</button>
           <button mat-menu-item (click)="openModal('signUp')">Registrarse</button>
@@ -35,6 +62,10 @@ import { SidenavService } from '../services/sidenav.service';
       </div>
 
     </mat-toolbar>
+
+
+
+    <!-- Toolbar mobile -->
 
     <mat-toolbar class="toolbar-menu-responsive">
 
@@ -59,7 +90,9 @@ import { SidenavService } from '../services/sidenav.service';
 `,
   styleUrls: ['./pages.component.scss']
 })
-export class PagesComponent {
+export class PagesComponent implements AfterViewInit {
+
+  @ViewChild('cart', {static: false}) btnCart: ElementRef<HTMLImageElement>
 
   buttons: Array<any> = [
     { path: '/productos', name: 'Productos', link: '/product-detail'},
@@ -67,6 +100,21 @@ export class PagesComponent {
     { path: '/ocasion', name: 'Para la ocasión', link: '/menu-ocasion'},
     { path: '/contacto', name: 'Contactanos', link: '/'},
   ];
+
+  itemsDummy = [
+    {name: 'Vodka Absolute Mango', category: 'Vodka', price: 100, stock: 3, description: 'description', img: [
+      '../../assets/images/demo/botella1.jpg' 
+    ] },
+    {name: 'Rose Wines 750 ml', category: 'Tequila', price: 100, stock: 3, description: 'description', img: [
+      '../../assets/images/demo/botella2.jpg'
+    ] },
+    {name: 'Lagar de Proventus', category: 'Vino tinto', price: 100, stock: 3, description: 'description', img: [
+      '../../assets/images/demo/botella3.jpg'
+    ] },
+    {name: 'Jack Daniels', category: 'Whiskey', price: 100, stock: 3, description: 'description', img: [
+      '../../assets/images/demo/botella4.jpg'
+    ] },
+  ]
 
   constructor(public dialog: MatDialog, private _sidenavService: SidenavService) {}
 
@@ -91,6 +139,10 @@ export class PagesComponent {
         console.log(`Dialog result: ${result}`);
       });
     }
+
+  }
+
+  ngAfterViewInit() {
 
   }
 

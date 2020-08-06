7b6578c3185b0a2fd7d55a03c353711a9669ac41
jaredDelao <jaredhm07@gmail.com>
2020-08-05 23:53:20 -0500

    funcionalidad profile

--- src/app/pages/@components-logged/profile/profile.component.ts
+++ src/app/pages/@components-logged/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  @ViewChild('inputName', {static: false}) inputName: ElementRef<HTMLInputElement>;
+  @ViewChild('inputMail', {static: false}) inputMail: ElementRef<HTMLInputElement>;
+  @ViewChild('inputTelefono', {static: false}) inputTelefono: ElementRef<HTMLInputElement>;
+  @ViewChild('inputPassword', {static: false}) inputPassword: ElementRef<HTMLInputElement>;
+
+  Fnombre: boolean = false;
+  Fcorreo: boolean = false;
+  Ftelefono: boolean = false;
+  Fpassword: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  editField(param: string, input: string) {
+
+    if (this[param] === false) {
+      this[param] = true;
+      setTimeout(() => {
+        this[input].nativeElement.select();
+      },100)
+    } else {
+      this[param] = false;
+    }
   }
 
 }
